fe8336dcbb80d7e4bb330213a050f11478d3ed55
Kimmelb-sg <kim.soubiegiroux@student.unimelb.edu.au>
2025-03-04 16:39:49 +1100

    Trying to push, I'm annoyed

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -3,12 +3,13 @@ package example;
 public class Calculator {
     public int add(int a, int b) {
         // TODO: Implement this method
-        return 5;
+        //Trying to push again
+        return a + b;
     }
 
     public int minus(int a, int b) {
         // TODO: Implement this method
-        return 10;
+        return a - b;
     }
 
     public int multiply(int a, int b) {
